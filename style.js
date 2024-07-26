74230967e83e703696c10eafd78022ce84a7ebfe
MD-Arman-Hosen <arman15-3304@diu.edu.bd>
2024-07-27 03:26:59 +0600

    final commit

---
+++ style.js
@@ -0,0 +1,35 @@
+// card-one
+document.getElementById('product-1').addEventListener('mouseover',function(){
+    document.body.style.backgroundColor = '#D9D898';
+})
+document.getElementById('product-1').addEventListener('mouseout',function(){
+    document.body.style.backgroundColor = 'white';
+})
+
+// card-two
+
+document.getElementById('product-2').addEventListener('mouseover',function(){
+    document.body.style.backgroundColor = '#CE8D74';
+})
+document.getElementById('product-2').addEventListener('mouseout',function(){
+    document.body.style.backgroundColor = 'white';
+})
+
+// card-three
+
+document.getElementById('product-3').addEventListener('mouseover',function(){
+    document.body.style.backgroundColor = '#DEC5AC';
+})
+document.getElementById('product-3').addEventListener('mouseout',function(){
+    document.body.style.backgroundColor = 'white';
+})
+
+// card-four
+document.getElementById('product-4').addEventListener('mouseover',function(){
+    document.body.style.backgroundColor = '#D47F3D';
+})
+document.getElementById('product-4').addEventListener('mouseout',function(){
+    document.body.style.backgroundColor = 'white';
+})
+
+
